baf07505b36d691bd6e4b1c06779d3198a662835
1139258234@qq.com <1139258234@qq.com>
2019-05-06 23:03:06 +0800

    123123123123

---
+++ cpp/main10.cpp
@@ -0,0 +1,44 @@
+/*
+There are N children standing in a line. Each child is assigned a rating value.
+
+You are giving candies to these children subjected to the following requirements:
+
+Each child must have at least one candy.
+Children with a higher rating get more candies than their neighbors.
+What is the minimum candies you must give?
+
+Example 1:
+
+Input: [1,0,2]
+Output: 5
+Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
+Example 2:
+
+Input: [1,2,2]
+Output: 4
+Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
+             The third child gets 1 candy because it satisfies the above two conditions. 
+
+*/
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+
+
+   
+        
+    }
+}; 
+int main(int argc, char const *argv[])
+{
+    
+    
+
+
+
+
+    return 0;
+}
