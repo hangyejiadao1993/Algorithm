a45b0a6302fb5253f3bc4708ae9daa0de5ea06bd
1139258234@qq.com <1139258234@qq.com>
2019-05-11 23:11:39 +0800

    131231231

--- cpp/main10.cpp
+++ cpp/main10.cpp
@@ -20,25 +20,50 @@ Explanation: You can allocate to the first, second and third child with 1, 2, 1 
              The third child gets 1 candy because it satisfies the above two conditions. 
 
 */
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int candy(vector<int>& ratings) {
-
-
-   
-        
+    int candy(vector<int> &ratings)
+    {
+        int len = ratings.size();
+        vector<int> candyNum(len, 1);
+        for (int i = 0; i < len; i++)
+        {
+            /* code */
+            if (ratings[i - 1] < ratings[i])
+            {
+                candyNum[i] = candyNum[i - 1] + 1;
+            }
+        }
+        for (int i = len - 2; i >= 0; i--)
+        {
+            /* code */
+            if (ratings[i] > ratings[i + 1] && candyNum[i] < candyNum[i + 1])
+            {
+                candyNum[i] = candyNum[i + 1] + 1;
+            }
+        }
+        int ret = 0;
+        for (int i = 0; i < len; i++)
+        {
+            /* code */
+            ret += candyNum[i];
+            std::cout << candyNum[i] << " ,";
+        }
+        std::cout << endl;
+        return ret;
     }
-}; 
+};
+
 int main(int argc, char const *argv[])
 {
-    
-    
-
-
-
+    int a[3] = {1, 2, 2};
+    vector<int> b(a, a + 3);
+    Solution *s = new Solution();
+    int re = s->candy(b);
 
     return 0;
 }
