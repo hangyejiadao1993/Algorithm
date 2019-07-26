71dc888125c10b628f04027a25d1667f81716086
hangyejiadao1993 <931917332@qq.com>
2019-07-26 19:28:56 +0800

    123

---
+++ cpp/main15.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+/*
+Given a Binary Search Tree and a target number, return true if there exist two elements in the BST such that their sum is equal to the given target.
+Example 1:
+Input: 
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+Target = 9
+Output: True
+Example 2:
+Input: 
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+Target = 28
+Output: False
+*/
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+} TreeNode;
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        
+    }
+};
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
+
+ 
