2ad7a901c50a2cfca6fe932ed9ae3b40e8bf1749
hangyejiadao1993 <931917332@qq.com>
2019-07-26 20:35:24 +0800

    12313

--- cpp/main15.cpp
+++ cpp/main15.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 /*
@@ -22,23 +23,69 @@ Input:
 Target = 28
 Output: False
 */
+
 struct TreeNode
 {
     int val;
     TreeNode *left;
     TreeNode *right;
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-} TreeNode;
-class Solution {
+};
+
+ class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
+        // One way is to convert the BST to a sorted list
+        // and then find the two elements going
+        // through the list
+        if (root == NULL) {
+            return false;
+        }
+        vector<int> v;
+        preOrderTraversal(root, v);
         
+        int startIdx = 0;
+        int endIdx = v.size()-1;
+        
+        while (startIdx < endIdx) {
+            if (v[startIdx] + v[endIdx] == k) {
+                return true;
+            }
+            if (v[startIdx] + v[endIdx] > k) {
+                endIdx -= 1;
+            } else {
+                startIdx += 1;
+            }
+        }
+        return false;
+    }
+    
+    void preOrderTraversal(TreeNode *root, vector<int>& v) {
+        if (root == NULL) {
+            return;
+        }
+        preOrderTraversal(root->left, v);
+        v.push_back(root->val);
+        preOrderTraversal(root->right, v);
     }
 };
+
 int main(int argc, char const *argv[])
 {
 
+    int arr[] = {2, 0, 3, -4, 1};
+
+    for (size_t i = 0; i < 5; i++)
+    {
+        for (size_t j = i + 1; j < 5; j++)
+        {
+
+            if (arr[i] + arr[j] == -1)
+            {
+                cout << "True" << endl;
+            }
+        }
+    }
+
     return 0;
 }
-
- 
