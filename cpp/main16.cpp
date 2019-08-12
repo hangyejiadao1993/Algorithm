65ef201200e1872f9fde4dca7a865f335ff96c03
hangyejiadao1993 <931917332@qq.com>
2019-08-12 12:47:41 +0800

    12312313

---
+++ cpp/main16.cpp
@@ -0,0 +1,28 @@
+/*
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+ */
+
+#include<iostream>
+using namespace std;
+class Solution
+{
+private:
+    /* data */
+public:
+    Solution(/* args */);
+    ~Solution();
+};
+
+ 
+ 
+
+int main(int argc, char const *argv[])
+{
+    
+    
+  
+
+    return 0;
+}
