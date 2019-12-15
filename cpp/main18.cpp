4291be9850546d187cbf749c8b86438c2f92b6e0
hangyejiadao1993 <1139258234@qq.com>
2019-12-15 13:45:34 +0800

    123

---
+++ cpp/main18.cpp
@@ -0,0 +1,40 @@
+/*
+两个栈实现一个队列2
+*/
+#include<stack>
+using namespace std;
+template<typenmae T>class CQueue
+{
+    public:
+        CQUueue(void);
+        ~CQueue(void);
+
+        void appendTail(const T& node);
+        T deleteHead();
+    private:
+        stack<T> stack1;
+        stack<T> stack2;
+}
+
+template<typename T>void CQueue<T>::appendTail(const T& element){
+    stack1.push(element);
+}
+
+template<typename T>T CQueue<T>::deleteHead(){
+    if (stack2.size()<=0)
+    {
+        while (stack1.size()<0)
+        {
+            T& data=stack1.top();
+            stack1.pop();
+            stack1.push(data);
+        }
+    }
+    if (stack2.size()==0)
+        throw new exception("queue is empty");
+    T head=stack2.top();
+    stack2.pop();
+    return head;
+    
+    
+}
